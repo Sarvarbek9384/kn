c06991dea070db8ef2a15ce18af2963a515067bc
Sarvarbek9384 <sahsahamidov2005.9384@gmail.com>
2024-02-08 12:33:35 +0300

    сафилисевна пушит

---
+++ WpfApp6/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp6
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Button[] buttons;
+        public MainWindow()
+        {
+            InitializeComponent();
+            buttons = new Button[9] { _1, _2, _3, _4, _5, _6, _7, _8, _9 };
+        }
+
+        private void _1_click(object sender, RoutedEventArgs e)
+        {
+            (sender as Button).Content = "x";
+            (sender as Button).IsEnabled = false;
+
+
+
+            Random random = new Random();
+            int nomerknopki = random.Next(0,9);
+
+
+            while (buttons[nomerknopki].IsEnabled == false)
+                nomerknopki = random.Next(0,9) ;
+
+
+            buttons[nomerknopki].Content = "0";
+            buttons[nomerknopki].IsEnabled = false;
+
+
+        }
+    }
+}
